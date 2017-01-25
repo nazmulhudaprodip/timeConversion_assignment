3dcc64662e9fdd9c489cc1313bd3182a7207b327
Nazmul Huda <nhu@trustpilot.comnhu@trustpilot.com>
2017-01-25 17:58:13 +0600

    Time conversion assignment

---
+++ AssignmentNet/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Assignment
+{
+    class TimeConversion
+    {
+        static void Main(string[] args)
+        {
+            string strTime = Console.ReadLine();
+            Console.WriteLine(Converter(strTime));
+            Console.ReadKey(true);
+        }
+
+        public static string Converter(string strTime)
+        {
+            string[] timeArr = strTime.Split(':');
+            string AMPM = timeArr[2].Substring(2);
+            string hh = string.Empty;
+            if (AMPM.Contains("PM"))
+            {
+                if (timeArr[0] == "12")
+                {
+                    hh = timeArr[0];
+                }
+
+                else
+                {
+                    hh = (Convert.ToInt64(timeArr[0]) + 12).ToString();
+                }
+            }
+            else if (AMPM.Contains("AM"))
+            {
+                if (timeArr[0] == "12")
+                {
+                    hh = "00";
+                }
+                else
+                {
+                    hh = timeArr[0];
+                }
+
+            }
+
+            return string.Concat(hh, ':', timeArr[1], ':', timeArr[2].Substring(0, 2));
+        }
+    }
+}
+
